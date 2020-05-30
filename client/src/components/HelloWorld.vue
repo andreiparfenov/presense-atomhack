91f1ad8133c7175f177ea36bf77a48adfb373ae4
Andrey <andreymail98@gmail.com>
2020-05-30 10:08:28 +0300

    create map

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main">
+    <div class="main-map">
+      <yandex-map :coords="coords" :zoom="15" @click="onClick" :options="options" :controls="controls">
+        <ymap-marker :coords="coords" marker-id="123" :hint-content="coords" />
+      </yandex-map>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      coords: [ 54.817533, 58.448946 ],
+      options: {
+        restrictMapArea: [
+            [54.83381208998252,58.4077226876815],
+            [54.79925718380296,58.4894802010907]
+        ]
+      },
+      controls: ['zoomControl']
+    }
+  },
+  methods: {
+    onClick (e) {
+      this.coords = e.get('coords')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.main-map {
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+ }
+
+ .ymap-container {
+    width: 100%;
+    height: 100%;
+ }
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import YmapPlugin from 'vue-yandex-maps'
+
+const settings = {
+  apiKey: '78e1e097-c8b2-4e8d-a2c5-d79922dfe53d',
+  lang: 'ru_RU',
+  coordorder: 'latlong',
+  version: '2.1'
+}
+
+Vue.use(YmapPlugin, settings)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
